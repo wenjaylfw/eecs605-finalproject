71cb25d11bb9e8e20adfa3a55d971d594f5eef99
wenjaylfw <57123025+wenjaylfw@users.noreply.github.com>
2022-04-08 15:10:13 -0400

    Dropdown Demos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import React from 'react';
 
+// global variables to change where necessary
+const DROPDOWN_API_ENDPOINT = 'https://80tl9latra.execute-api.us-east-1.amazonaws.com/prod/'; // TODO
+const ML_API_ENDPOINT = 'https://80tl9latra.execute-api.us-east-1.amazonaws.com/prod/'; // TODO
+
+
 // atob is deprecated but this function converts base64string to text string
 const decodeFileBase64 = (base64String) => {
   // From Bytestream to Percent-encoding to Original string
@@ -17,6 +22,12 @@ function App() {
   const [outputFileData, setOutputFileData] = React.useState(''); // represented as readable data (text string)
   const [buttonDisable, setButtonDisable] = React.useState(true);
   const [buttonText, setButtonText] = React.useState('Submit');
+  const [submitButtonText, setSubmitButtonText] = React.useState('Submit');
+  const [fileButtonText, setFileButtonText] = React.useState('Upload File');
+  const [demoDropdownFiles, setDemoDropdownFiles] = React.useState([]);
+  const [selectedDropdownFile, setSelectedDropdownFile] = React.useState('');
+  const [inputImage, setInputImage] = React.useState(''); // represented as bytes data (string)
+
 
   // convert file to bytes data
   const convertFileToBytes = (inputFile) => {
@@ -34,6 +45,25 @@ function App() {
       };
     });
   }
+  
+  // make GET request to get demo files on load -- takes a second to load
+  React.useEffect(() => {
+    fetch(DROPDOWN_API_ENDPOINT)
+    .then(response => response.json())
+    .then(data => {
+      // GET request error
+      if (data.statusCode === 400) {
+        console.log('Sorry! There was an error, the demo files are currently unavailable.')
+      }
+
+      // GET request success
+      else {
+        const s3BucketFiles = JSON.parse(data.body);
+        setDemoDropdownFiles(s3BucketFiles["s3Files"]);
+      }
+    });
+  }, [])
+
 
   // handle file input
   const handleChange = async (event) => {
@@ -122,11 +152,56 @@ function App() {
     .then(() => {
       console.log('POST request success');
     })
+    
+    
+      // handle demo dropdown file selection
+  const handleDropdown = (event) => {
+    setSelectedDropdownFile(event.target.value);
+
+    // temporarily disable submit button
+    setButtonDisable(true);
+    setSubmitButtonText('Loading Demo File...');
+
+    // only make POST request on file selection
+    if (event.target.value) {
+      fetch(DROPDOWN_API_ENDPOINT, {
+        method: 'POST',
+        body: JSON.stringify({ "fileName": event.target.value })
+      }).then(response => response.json())
+      .then(data => {
+
+        // POST request error
+        if (data.statusCode === 400) {
+          console.log('Uh oh! There was an error retrieving the dropdown file from the S3 bucket.')
+        }
+
+        // POST request success
+        else {
+          const dropdownFileBytesData = JSON.parse(data.body)['bytesData'];
+          setInputFileData(dropdownFileBytesData);
+          setInputImage('data:image/png;base64,' + dropdownFileBytesData); // hacky way of setting image from bytes data - even works on .jpeg lol
+          setSubmitButtonText('Submit');
+          setButtonDisable(false);
+        }
+      });
+    }
+
+    else {
+      setInputFileData('');
+    }
+  }
+
   }
   return (
     <div className="App">
       <div className="Input">
         <h1>Input</h1>
+        <label htmlFor="demo-dropdown">Demo: </label>
+        <select name="Select Image" id="demo-dropdown" value={selectedDropdownFile} onChange={handleDropdown}>
+            <option value="">-- Select Demo File --</option>
+            {demoDropdownFiles.map((file) => <option key={file} value={file}>{file}</option>)}
+        </select>
+
         <form onSubmit={handleSubmit}>
           <input type="file" accept=".wav" onChange={handleChange} />
           <button type="submit" disabled={buttonDisable}>{buttonText}</button>
